465a896b2373bea8222ba647b1f66cb3b8a55bbc
samuel.mofrad <samuel@unus.one>
2019-12-23 23:11:48 +0100

    Add Vazir font and change html lang and dir

--- header.php
+++ header.php
@@ -6,16 +6,15 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package zuari
+ * @package zuari => mando
  */
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html dir="rtl" lang="fa" <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
 </head>
